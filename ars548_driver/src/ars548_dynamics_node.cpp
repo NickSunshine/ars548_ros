85d52914d4a51f8ec62c7ab5e69b760498420cfd
Nick Sunshine <nsunshine@vtti.vt.edu>
2025-07-14 12:09:15 -0400

    Adds support for driving direction based on gear state.

--- ars548_driver/src/ars548_dynamics_node.cpp
+++ ars548_driver/src/ars548_dynamics_node.cpp
@@ -1,19 +1,58 @@
-
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/Float64.h>
 #include <dbw_mkz_msgs/SteeringReport.h>
+#include <dbw_mkz_msgs/GearReport.h>
+#include <dbw_mkz_msgs/Gear.h>
+
 #include <mutex>
 
 sensor_msgs::Imu latest_imu_msg;
 std_msgs::Float64 latest_speed_msg;
 dbw_mkz_msgs::SteeringReport latest_steering_msg;
+dbw_mkz_msgs::GearReport latest_gear_report_msg;
+uint8_t latest_gear = dbw_mkz_msgs::Gear::NONE;
+DrivingDirection latest_direction = DrivingDirection::Standstill;
 std::mutex imu_mutex;
 std::mutex speed_mutex;
 std::mutex steering_mutex;
+std::mutex gear_mutex;
 bool imu_msg_received = false;
 bool speed_msg_received = false;
 bool steering_msg_received = false;
+bool gear_msg_received = false;
+
+// Enum for driving direction
+enum class DrivingDirection {
+    Standstill = 0,
+    Forward = 1,
+    Backward = 2
+};
+
+// Helper function to convert DrivingDirection to string
+std::string directionToString(DrivingDirection dir)
+{
+    switch (dir) {
+        case DrivingDirection::Standstill: return "Standstill";
+        case DrivingDirection::Forward: return "Forward";
+        case DrivingDirection::Backward: return "Backward";
+        default: return "Unknown";
+    }
+}
+
+// Helper function to convert dbw_mkz_msgs::Gear enum to string
+std::string gearToString(uint8_t gear)
+{
+    switch (gear) {
+        case dbw_mkz_msgs::Gear::NONE: return "NONE";
+        case dbw_mkz_msgs::Gear::PARK: return "PARK";
+        case dbw_mkz_msgs::Gear::REVERSE: return "REVERSE";
+        case dbw_mkz_msgs::Gear::NEUTRAL: return "NEUTRAL";
+        case dbw_mkz_msgs::Gear::DRIVE: return "DRIVE";
+        case dbw_mkz_msgs::Gear::LOW: return "LOW";
+        default: return "UNKNOWN";
+    }
+}
 
 void IMUCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
@@ -39,6 +78,14 @@ void SteeringCallback(const dbw_mkz_msgs::SteeringReport::ConstPtr& msg)
     // Further steering data processing here
 }
 
+void GearCallback(const dbw_mkz_msgs::GearReport::ConstPtr& msg)
+{
+    std::lock_guard<std::mutex> lock(gear_mutex);
+    latest_gear_report_msg = *msg;
+    gear_msg_received = true;
+    // Further gear data processing here
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "ars548_dynamics_node");
@@ -49,9 +96,13 @@ int main(int argc, char** argv)
     std::string imu_topic;
     std::string speed_topic;
     std::string steering_topic;
+    double steering_gear_ratio = 14.81;
+    std::string gear_topic;
     nh.param<std::string>("imu_topic", imu_topic, "");
     nh.param<std::string>("speed_topic", speed_topic, "");
     nh.param<std::string>("steering_topic", steering_topic, "");
+    nh.param<std::string>("gear_topic", gear_topic, "");
+    nh.param<double>("steering_gear_ratio", steering_gear_ratio, 14.81);
 
     ros::Subscriber imu_sub;
     if (imu_topic.empty()) {
@@ -75,6 +126,14 @@ int main(int argc, char** argv)
     } else {
         steering_sub = nh.subscribe(steering_topic, 1, SteeringCallback);
         ROS_INFO_STREAM("Subscribed to steering topic: " << steering_topic);
+    }
+
+    ros::Subscriber gear_sub;
+    if (gear_topic.empty()) {
+        ROS_ERROR("Parameter 'gear_topic' is not set. Skipping gear subscription.");
+    } else {
+        gear_sub = nh.subscribe(gear_topic, 1, GearCallback);
+        ROS_INFO_STREAM("Subscribed to gear topic: " << gear_topic);
     }
 
     ros::Rate loop_rate(20); // 20 Hz
@@ -107,9 +166,36 @@ int main(int argc, char** argv)
         {
             std::lock_guard<std::mutex> lock(steering_mutex);
             if (steering_msg_received) {
-                // Convert steering wheel angle rad to steering front axle degrees (14.81:1 ratio)
-                double front_wheel_angle_deg = latest_steering_msg.steering_wheel_angle * (180.0 / M_PI) / 14.81; 
-                ROS_INFO_STREAM("[Loop] Latest front wheel angle: " << front_wheel_angle_deg << " degrees");
+                // Convert steering wheel angle rad to steering front axle degrees using parameterized gear ratio
+                double front_wheel_angle_deg = latest_steering_msg.steering_wheel_angle * (180.0 / M_PI) / steering_gear_ratio;
+                ROS_INFO_STREAM("[Loop] Latest front wheel angle: " << front_wheel_angle_deg << " degrees (gear ratio: " << steering_gear_ratio << ")");
+            }
+        }
+
+
+        // Example: Access the latest gear data safely
+        {
+            std::lock_guard<std::mutex> lock(gear_mutex);
+            if (gear_msg_received) {
+                latest_gear = latest_gear_report_msg.gear;
+                std::string gear_str = gearToString(latest_gear);
+                // Map gear to driving direction
+                switch (latest_gear) {
+                    case dbw_mkz_msgs::Gear::DRIVE:
+                    case dbw_mkz_msgs::Gear::LOW:
+                        latest_direction = DrivingDirection::Forward;
+                        break;
+                    case dbw_mkz_msgs::Gear::REVERSE:
+                        latest_direction = DrivingDirection::Backward;
+                        break;
+                    case dbw_mkz_msgs::Gear::PARK:
+                    case dbw_mkz_msgs::Gear::NEUTRAL:
+                    case dbw_mkz_msgs::Gear::NONE:
+                    default:
+                        latest_direction = DrivingDirection::Standstill;
+                        break;
+                }
+                ROS_INFO_STREAM("[Loop] Latest gear state: " << gear_str << " (" << static_cast<int>(latest_gear) << ") | Driving direction: " << directionToString(latest_direction) << " (" << static_cast<int>(latest_direction) << ")");
             }
         }
 
